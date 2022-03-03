fdfc10c54a81bc1f5938bdc35254852589e0201a
UT0P1C <ljonham@gmail.com>
2022-03-03 14:36:05 -0300

    função do hino do vasco

--- index.js
+++ index.js
@@ -24,20 +24,33 @@ client.on("message", async (msg) => {
 	//comandos
 
 	if(msg.content === prefixo + "brota"){
-		await msg.member.voice.channel.join(); // $brota
-		msg.channel.send("Brotei");
+		if(msg.member.voice.channel){
+			servidores.server.connection = await msg.member.voice.channel.join(); // $brota
+			msg.channel.send("Brotei");
+		}else if(!msg.member.voice.channel){
+			msg.channel.send("Vai pro canal de voz antes de me chamar doente");
+		}
 	}
 
 	if(msg.content === prefixo + "vaza"){
-
+					//$vaza
 	}
 
 	if(msg.content === prefixo + "ajuda"){
-		msg.channel.send("Comandos atuais: $brota, $pao de cria");
+		msg.channel.send("Comandos atuais: $brota, $pao de cria"); //$ajuda
 	}
 
 	if(msg.content === prefixo + "pao de cria"){
-		msg.channel.send("não, de sal");
+		msg.channel.send("não, de sal"); // $pao de cria
+	}
+	
+	if(msg.content === prefixo + "vasco"){
+		if(msg.member.voice.channel){
+			servidores.server.connection = await msg.member.voice.channel.join();
+			servidores.server.connection.play("./vasco.mp3"); //$vasco
+		}else if(!msg.member.voice.channel){
+			msg.channel.send("Vai pro canal de voz antes de me chamar doente");
+		} 
 	}
 
 });
