91de4ccf31d9dd2d70fc9a14265e8c816a16bee0
UT0P1C <ljonham@gmail.com>
2022-03-03 15:18:23 -0300

    adicionando função de tocar no youtube

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const ytdl = require("ytdl-core");
 
 const TOKEN = "OTQ4OTcyMjczOTg4MTczODQ0.YiDlTQ.9kXP0GR_HYERokZE9bteyxoCA3M";
 
@@ -8,7 +9,7 @@ const prefixo = "$";
 const servidores = {
 	'server': {
 		connection: null,
-		despatcher: null
+		dispatcher: null
 	}
 }
 
@@ -29,6 +30,7 @@ client.on("message", async (msg) => {
 			msg.channel.send("Brotei");
 		}else if(!msg.member.voice.channel){
 			msg.channel.send("Vai pro canal de voz antes de me chamar doente");
+			return;
 		}
 	}
 
@@ -46,10 +48,35 @@ client.on("message", async (msg) => {
 	
 	if(msg.content === prefixo + "vasco"){
 		if(msg.member.voice.channel){
+			msg.channel.send("O CAMPEÃO DOS CAMPEÕES!!!!");
 			servidores.server.connection = await msg.member.voice.channel.join();
 			servidores.server.connection.play("./vasco.mp3"); //$vasco
 		}else if(!msg.member.voice.channel){
 			msg.channel.send("Vai pro canal de voz antes de me chamar doente");
+			return;
+		} 
+	}
+
+	if(msg.content.startsWith(prefixo + "toca")){
+		if(msg.member.voice.channel){
+			
+			let music = msg.content.slice(6);
+
+			if(ytdl.validateURL(music)){
+				servidores.server.connection = await msg.member.voice.channel.join();
+				
+				servidores.server.dispatcher = servidores.server.connection.play(ytdl(music)); //$toca <link>
+
+				msg.channel.send("Toquei");
+			}else{
+				msg.channel.send("link inválido");
+			}
+
+		}else if(!msg.member.voice.channel){
+			
+			msg.channel.send("Vai pro canal de voz antes de me chamar doente");
+			return;
+		
 		} 
 	}
 
