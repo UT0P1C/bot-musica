a707459474417d36eb251e55e240fb5b11bd5eb5
UT0P1C <ljonham@gmail.com>
2022-03-03 14:19:39 -0300

    adicionando bibliotecas e comandos

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const Discord = require("discord.js");
-const client = Discord.Client();
+const client = new Discord.Client();
 
 const TOKEN = "OTQ4OTcyMjczOTg4MTczODQ0.YiDlTQ.9kXP0GR_HYERokZE9bteyxoCA3M";
+
+const prefixo = "$";
+
+const servidores = {
+	'server': {
+		connection: null,
+		despatcher: null
+	}
+}
 
 client.on("ready", () => {
 	console.log("cheguei nessa porra");
 });
 
-client.on("message", (msg) => {
+client.on("message", async (msg) => {
+	//filtros
+	if(!msg.guild) return;
+	if(!msg.content.startsWith(prefixo)) return;
+
+	//comandos
+
+	if(msg.content === prefixo + "brota"){
+		await msg.member.voice.channel.join(); // $brota
+		msg.channel.send("Brotei");
+	}
+
+	if(msg.content === prefixo + "vaza"){
+
+	}
+
+	if(msg.content === prefixo + "ajuda"){
+		msg.channel.send("Comandos atuais: $brota, $pao de cria");
+	}
+
+	if(msg.content === prefixo + "pao de cria"){
+		msg.channel.send("não, de sal");
+	}
 
 });
 
